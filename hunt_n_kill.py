0e496be5e1c264d0712e0857972546a38e529aca
Ilias Kolokousis <ikolokou@c3a6c1.42berlin.de>
2026-04-24 21:58:25 +0200

    Show maze generation animation

--- hunt_n_kill.py
+++ hunt_n_kill.py
@@ -3,6 +3,9 @@
 import random
 from maze_class import Maze
 from solve_maze import solve_maze
+from time import sleep
+from viz import Viz
+import sys
 
 
 def _get_x_y(maze: Maze) -> tuple[int]:
@@ -19,6 +22,32 @@ def _get_x_y(maze: Maze) -> tuple[int]:
     x: int = random.randint(0, maze.width - 1)
     y: int = random.randint(0, maze.height - 1)
     return (x, y)
+
+
+def create_output_file(maze: Maze) -> None:
+    grid: list[list[str]] = [row[:] for row in maze.grid]
+    entry: str = f'{maze.entry[0]},{maze.entry[1]}'
+    exit: str = f'{maze.exit[0]},{maze.exit[1]}'
+
+    for row in range(maze.height):
+        for cell in range(maze.width):
+            grid[row][cell] = str(hex(maze.grid[row][cell]))[2:].upper()
+
+    output: str = ''
+    for row in grid:
+        output += ''.join(row) + '\n'
+    output += f'\n{entry}\n{exit}\n'
+    with open(maze.output_file, 'w') as f:
+        f.write(output)
+
+
+def print_state(maze: Maze) -> None:
+    create_output_file(maze)
+    sleep(0.05)
+    print("\033[H", end="", flush=True)
+    viz: Viz = Viz(maze.output_file, maze.coords_42)
+    viz.render(viz.output)
+    print("\033[J")
 
 
 def _produce_neighbors(maze: Maze, start: tuple) -> list[tuple]:
@@ -131,6 +160,113 @@ def _kill(maze: Maze, start: tuple, visited: set[tuple]) -> tuple:
     return next_cell
 
 
+def get_input(maze: Maze, solution_rendered: bool = False) -> None:
+    print("""
+=== A-Maze-ing ===
+\t1. Regenerate a new maze
+\t2. Show/Hide path from entry to exit
+\t3. Choose new colors
+\t4. Quit
+          """)
+    choice: str = input("Choice? (1-4): ")
+    if choice == '1':
+        print("\033[2J\033[H", end="", flush=True)
+        create_maze()
+    elif choice == '2':
+        viz: Viz = Viz(maze.output_file, maze.coords_42)
+        if solution_rendered:
+            print("\033[2J\033[H", end="", flush=True)
+            viz.render(viz.output)
+            get_input(maze)
+        else:
+            print("\033[2J\033[H", end="", flush=True)
+            viz.render(viz.output_w_solution)
+            get_input(maze, True)
+    elif choice == '3':
+        viz: Viz = Viz(maze.output_file, maze.coords_42)
+        print("==========")
+        print("""
+Choose the # corresponding to the desired color:
+\t1. Black
+\t2. Red
+\t3. Green
+\t4. Yellow
+\t5. Blue
+\t6. Purple
+\t7. Cyan
+\t8. White
+            """)
+        walls: str = input('Walls (choice: 1-8): ')
+        path: str = input('Path (choice: 1-8): ')
+        start: str = input('Start (choice: 1-8): ')
+        end: str = input('End (choice: 1-8): ')
+        fourty_two: str = input('42 (choice: 1-8): ')
+        solution: str = input('Solution (choice: 1-8): ')
+        answers: list[str] = [walls, path, start, end, fourty_two, solution]
+        for answer in answers:
+            if int(answer.strip('.')) not in set(range(1, 9)):
+                print("\033[2J\033[H", end="", flush=True)
+                if solution_rendered:
+                    viz.render(viz.output_w_solution)
+                    print("""
+        \033[1;31mPlease put a number in from 1 to 8 only in colors\033[0m
+                    """)
+                    get_input(maze, True)
+                    return
+                else:
+                    viz.render(viz.output)
+                    print("""
+        \033[1;31mPlease put a number in from 1 to 8 only in colors\033[0m
+                    """)
+                    get_input(maze)
+                    return
+
+        answer_dict: dict[str, str] = {
+            '1': 'Black',
+            '2': 'Red',
+            '3': 'Green',
+            '4': 'Yellow',
+            '5': 'Blue',
+            '6': 'Purple',
+            '7': 'Cyan',
+            '8': 'White'
+        }
+
+        viz.CELL_COLORS['wall'] = viz.COLORS[answer_dict[walls.strip('.')]]
+        viz.CELL_COLORS['path'] = viz.COLORS[answer_dict[path.strip('.')]]
+        viz.CELL_COLORS['start'] = viz.COLORS[answer_dict[start.strip('.')]]
+        viz.CELL_COLORS['end'] = viz.COLORS[answer_dict[end.strip('.')]]
+        viz.CELL_COLORS['coord'] = viz.COLORS[
+            answer_dict[fourty_two.strip('.')]]
+        viz.CELL_COLORS['solved'] = viz.COLORS[
+            answer_dict[solution.strip('.')]]
+
+        print("\033[2J\033[H", end="", flush=True)
+        if solution_rendered:
+            viz.render(viz.output_w_solution)
+            get_input(maze, True)
+        else:
+            viz.render(viz.output)
+            get_input(maze)
+    elif choice == '4':
+        return
+    else:
+        print("\033[2J\033[H", end="", flush=True)
+        viz: Viz = Viz(maze.output_file, maze.coords_42)
+        if solution_rendered:
+            viz.render(viz.output_w_solution)
+            print("""
+\033[1;31mPlease put a number in from 1 to 4 only\033[0m
+              """)
+            get_input(maze, True)
+        else:
+            viz.render(viz.output)
+            print("""
+\033[1;31mPlease put a number in from 1 to 4 only\033[0m
+              """)
+            get_input(maze)
+
+
 def create_maze() -> None:
     """The main logic behind the hunt and kill algorithm. It first
     initializes the maze object. Then, it finds a random start in the grid
@@ -139,33 +275,55 @@ def create_maze() -> None:
     visited) or finds a cell that is unvisited and starts from there
     carving new walls.
     """
-    maze: Maze = Maze('custom_config.ini')
-    start: tuple[int] = _get_x_y(maze)
-    while start in maze.coords_42:
-        start = _get_x_y(maze)
-    visited: set[tuple] = {start}
+    try:
+        maze: Maze = Maze('custom_config.ini')
+        start: tuple[int] = _get_x_y(maze)
+        while start in maze.coords_42:
+            start = _get_x_y(maze)
+        visited: set[tuple] = {start}
 
-    while len(visited) + len(maze.coords_42) < maze.width * maze.height:
-        neighbors: list[tuple] = _produce_neighbors(maze, start)
+        print("\033[2J\033[H", end="", flush=True)
+        while len(visited) + len(maze.coords_42) < maze.width * maze.height:
+            print_state(maze)
+            neighbors: list[tuple] = _produce_neighbors(maze, start)
 
-        if all(n in visited for n in neighbors):
-            start = _hunt(maze, start, visited)
-        start = _kill(maze, start, visited)
+            if all(n in visited for n in neighbors):
+                start = _hunt(maze, start, visited)
+            start = _kill(maze, start, visited)
 
-    entry: str = f'{maze.entry[0]},{maze.entry[1]}'
-    exit: str = f'{maze.exit[0]},{maze.exit[1]}'
-    path: str = ''.join(solve_maze(maze))
-    # print(len(path))
+        print_state(maze)
+        grid: list[list[str]] = [row[:] for row in maze.grid]
+        entry: str = f'{maze.entry[0]},{maze.entry[1]}'
+        exit: str = f'{maze.exit[0]},{maze.exit[1]}'
+        path: str = ''.join(solve_maze(maze))
 
-    for row in range(maze.height):
-        for cell in range(maze.width):
-            maze.grid[row][cell] = str(hex(maze.grid[row][cell]))[2:].upper()
-    output = ''
-    for row in maze.grid:
-        output += ''.join(row) + '\n'
-    output += f'\n{entry}\n{exit}\n{path}'
-    with open(maze.output_file, 'w') as f:
-        f.write(output)
+        for row in range(maze.height):
+            for cell in range(maze.width):
+                grid[row][cell] = str(hex(maze.grid[row][cell]))[2:].upper()
+
+        output: str = ''
+        for row in grid:
+            output += ''.join(row) + '\n'
+        output += f'\n{entry}\n{exit}\n{path}'
+        with open(maze.output_file, 'w') as f:
+            f.write(output)
+
+        print("\033[H", end="", flush=True)
+        viz: Viz = Viz(maze.output_file, maze.coords_42)
+        viz.render(viz.output)
+        get_input(maze)
+
+    except Exception as e:
+        print("""
+Please make sure the file you is all below:
+    1. Exists with no typos in name.
+    2. Is in the correct path you supplied with no typos.
+    3. All permissions are configured correctly:
+        (bash) ls -l \t\t# to check permissions
+        (bash) chmod 777 . -R \t# to configure permissions
+    4. Is a text file.
+              """, file=sys.stderr)
+        print(e)
 
 
 if __name__ == "__main__":
