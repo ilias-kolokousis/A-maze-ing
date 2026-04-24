c821ebaaa9ea653d83bfc8d6dc3d876597a62bc2
Ilias Kolokousis <ikolokou@c3a6c1.42berlin.de>
2026-04-24 21:57:50 +0200

    Update for error handling

--- maze_class.py
+++ maze_class.py
@@ -2,7 +2,9 @@
 
 import random
 from configparser import ConfigParser
-from typing import List, Callable, TypeAlias
+from typing import Callable, TypeAlias
+from pathlib import Path
+from configuration import plot_42
 
 BitOp: TypeAlias = Callable[[int], int]
 
@@ -33,11 +35,13 @@ class Maze():
         Args:
             config_path (str): path to the config.ini file
         """
+        if not Path(config_path).exists():
+            raise Exception
         config: ConfigParser = ConfigParser()
         config.read(config_path)
         self.seed: int = config.get('custom', 'seed')
-        # if self.seed is not None:
-        #     random.seed(self.seed)
+        if self.seed is not None:
+            random.seed(self.seed)
         self.width: int = config.getint('custom', 'width')
         self.height: int = config.getint('custom', 'height')
         self.entry: tuple[int, ...] = tuple(
@@ -46,24 +50,31 @@ class Maze():
             int(x) for x in config.get('custom', 'exit').split(','))
         self.output_file: str = config.get('custom', 'output_file')
         self.perfect: bool = config.getboolean('custom', 'perfect')
-        self.coords_42: List[tuple[int, ...]] = [tuple(int(x)
-                                                 for x in pair.split(','))
-                                                 for pair in config.get(
-                                                     'custom', '42_coords')
-                                                 .split(':')]
         self.grid: list[list[int]] = [[15 for w in range(self.width)]
                                       for h in range(self.height)]
 
+    @property
+    def coords_42(self) -> list[tuple[int, ...]] | None:
+        if self.width < 9 or self.height < 7:
+            return None
+
+        pad_left: int = (self.width - 8) // 2
+        pad_top: int = (self.height - 5) // 2
+
+        return plot_42(pad_left, pad_top)
+
 
 if __name__ == '__main__':
-    maze: Maze = Maze('src/custom_config.ini')
+    maze: Maze = Maze('custom_config.ini')
     for key, value in maze.__dict__.items():
         if key == 'grid':
             initialized: bool = all(all(x == 15 for x in i) for i in value)
             correct_size: bool = (len(value) == maze.height
                                   and all(len(x) == maze.width
                                           for x in value))
-            print(f'Grid initialized and correct size: {initialized
-                                                        and correct_size}')
+            print(f"""
+Grid initialized and correct size: {initialized and correct_size}
+                    """)
         else:
             print(f'{key}: {value}')
+    print(f'coords_42: {maze.coords_42}')
